59282e8498e286eaeb2fad0a536006fa98ec9aea
chroth7 <chroth@me.com>
2022-01-03 11:52:06 +0100

    Some quick exercises.

---
+++ assembly-line/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let basic_production = 221.0;
+    let success_rate = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        _ => 0.77,
+    };
+
+    (speed as f64 * basic_production) * success_rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
