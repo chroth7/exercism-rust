6807e293ac2ebed67ee8a3c7149927c540a830a0
chroth7 <chroth@me.com>
2022-01-04 09:13:53 +0100

    Nicer RPN.

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -24,49 +24,37 @@ fn operate(
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut result = Vec::new();
     for input in inputs {
-        match input {
-            CalculatorInput::Value(n) => result.push(CalculatorInput::Value(*n)),
+        let new_val = match input {
+            CalculatorInput::Value(n) => Some(CalculatorInput::Value(*n)),
             CalculatorInput::Add => {
                 if result.len() < 2 {
                     return None;
                 }
-                let foo = operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| x + y);
-                match foo {
-                    None => return None,
-                    Some(n) => result.push(n),
-                }
+                operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| x + y)
             }
             CalculatorInput::Subtract => {
                 if result.len() < 2 {
                     return None;
                 }
-                let foo = operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| y - x);
-                match foo {
-                    None => return None,
-                    Some(n) => result.push(n),
-                }
+                operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| y - x)
             }
             CalculatorInput::Multiply => {
                 if result.len() < 2 {
                     return None;
                 }
-                let foo = operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| x * y);
-                match foo {
-                    None => return None,
-                    Some(n) => result.push(n),
-                }
+                operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| x * y)
             }
             CalculatorInput::Divide => {
                 if result.len() < 2 {
                     return None;
                 }
-                let foo = operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| y / x);
-                match foo {
-                    None => return None,
-                    Some(n) => result.push(n),
-                }
+                operate(result.pop().unwrap(), result.pop().unwrap(), |x, y| y / x)
             }
         };
+        match new_val {
+            Some(val) => result.push(val),
+            None => return None,
+        }
     }
 
     if result.len() != 1 {
