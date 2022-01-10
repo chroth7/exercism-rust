f4c6f68532496aa880d2f00c25726eddc3bf0098
chroth7 <chroth@me.com>
2022-01-10 21:41:06 +0100

    Add leap.

---
+++ leap/src/lib.rs
@@ -0,0 +1,14 @@
+use std::ops::Div;
+
+fn divides(n: u64, d: u64) -> bool {
+    n % d == 0
+}
+
+pub fn is_leap_year(year: u64) -> bool {
+    match year {
+        x if divides(x, 400) => true,
+        x if divides(x, 100) => false,
+        x if divides(x, 4) => true,
+        _ => false,
+    }
+}
