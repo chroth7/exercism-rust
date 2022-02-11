658050469c6145fc0ccb90dbfe15154f612a2e75
chroth7 <chroth@me.com>
2022-02-11 10:52:14 +0100

    Mini comment.

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,6 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    // I wanted to remove 0 early... one could also do that on each run in the filter below
+    let clean_factors: Vec<&u32> = factors.iter().filter(|&n| n > &0).collect();
+
     (1..limit)
         .into_iter()
-        .filter(|&n| factors.iter().any(|&factor| factor > 0 && n % factor == 0))
+        .filter(|&n| clean_factors.iter().any(|&factor| n % factor == 0))
         .sum()
 }
